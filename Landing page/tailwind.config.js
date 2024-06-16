139c126df9203b6c43b1a1dcaa6fa2909f7b11d7
azimiHamid <azimihamid2003@gmail.com>
2024-06-16 22:36:03 +0500

    CodSoft projects initial commit

---
+++ Landing page/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      padding: {
+        '9/16': '56.25%'
+      },
+    },
+  },
+  plugins: [],
+};
